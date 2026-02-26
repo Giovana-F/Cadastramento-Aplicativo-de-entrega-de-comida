4b612df44775280b83d45eeb0ed9876802d14648
Giovana-F <gi.fiorin.giusti@gmail.com>
2026-02-26 01:49:02 -0300

    codigo limpo

--- cadfood.py
+++ cadfood.py
@@ -11,16 +11,16 @@ print('2. Listar Restaurante')
 print('3. Ativar Restaurante')    
 print('4. Sair\n')
 
-opcao_escolhida =input('Escolha uma opcao: ')
+opcao_escolhida =int(input('Escolha uma opcao: ')) #o input ret uma string. Por isso a utilização do int, assim que o usuári inseri o valor, é feito a conversão
 print(f'Voce escolheu a opcao: {opcao_escolhida}')
 
-if opcao_escolhida == '1':
+if opcao_escolhida == 1:
     print("Cadastrar Restaurante")
-elif opcao_escolhida == '2':
+elif opcao_escolhida == 2:
     print("Listar Restaurante")
-elif opcao_escolhida == '3':
+elif opcao_escolhida == 3:
     print("Ativar Restaurante")
-elif opcao_escolhida == '4':
+elif opcao_escolhida == 4 :
     print("Encerrando o programa... ")
 else:
     print("Opcao Invalida")
