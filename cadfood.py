4c9f3250fb6ce9717aa1c101991de3aeb66fa937
Giovana-F <gi.fiorin.giusti@gmail.com>
2026-02-26 01:30:28 -0300

    opções de escolha

--- cadfood.py
+++ cadfood.py
@@ -13,3 +13,15 @@ print('4. Sair\n')
 
 opcao_escolhida =input('Escolha uma opcao: ')
 print(f'Voce escolheu a opcao: {opcao_escolhida}')
+
+if opcao_escolhida == '1':
+    print("Cadastrar Restaurante")
+elif opcao_escolhida == '2':
+    print("Listar Restaurante")
+elif opcao_escolhida == '3':
+    print("Ativar Restaurante")
+elif opcao_escolhida == '4':
+    print("Encerrando o programa... ")
+else:
+    print("Opcao Invalida")
+
